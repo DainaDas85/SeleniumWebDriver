8d34a76d1bd642ea585c7544bc2ebae9e484b52f
DainaDas85 <165977613+DainaDas85@users.noreply.github.com>
2024-04-04 09:44:39 -0400

    Add files via upload

---
+++ WebstaurantStore.java
@@ -0,0 +1,109 @@
+package objects;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
+public class WebstaurantStore {
+	WebDriver driver;
+
+//	Constructor
+	public WebstaurantStore(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+
+	}
+
+//	WebElements
+
+	@FindBy(id = "searchval")
+	WebElement searchInput;
+	
+	@FindBy(xpath = "(//*[@id=\"ProductBoxContainer\"]/div[4]/form/div/div/input[2])[58]")
+	WebElement addToCartButton;
+	
+	@FindBy(xpath = "//*[@id=\"watnotif-wrapper\"]/div/p/div[2]/div[2]/a[1]")
+	WebElement viewCart;
+	
+	@FindBy(css = "button[class='emptyCartButton btn btn-mini btn-ui pull-right']")
+	WebElement emptyCartButton;
+	
+	@FindBy(xpath = "//*[@id=\"td\"]/div[11]/div/div/div/footer/button[1]")
+	WebElement EmptyCartConfirmBtn;
+	
+	@FindBy(xpath = "//*[@id=\"main\"]/div/div[1]/div[1]/div/div[2]/p[1]")
+	WebElement validateEmptyCart;
+	
+	
+	
+//	Methods
+	
+	public void searchStainlessWorkTable(String input) throws InterruptedException {
+		searchInput.sendKeys(input);
+		searchInput.submit();
+		
+		System.out.println("TEST PASSED: Searched for " + input);
+		
+		Thread.sleep(2000);
+	}
+	
+	public void isProductTitleContainsTable() {
+		
+//		Find all the Product title
+		
+		List<WebElement> productTitles = driver.findElements(By.xpath("//*[@id=\"product_listing\"]"));
+		
+//		Check each product title to ensure it contains the word "table"
+
+		boolean allTitlesContainTable = true;
+		
+		for(WebElement title : productTitles) {
+			String titleText  = title.getText().toLowerCase();
+			
+			if(!titleText.contains("table")) {
+				allTitlesContainTable = false;
+				
+				System.out.println("Title doesn't contain the word 'table' : " + titleText);
+			}
+
+//			Check if all product titles contain the word "Table"
+			
+			if(allTitlesContainTable) {
+				System.out.println("TEST PASSED: All product's titles contain 'table'");
+			}else {
+				System.out.println("TEST FAILED: Not all product titles contain table.");
+			}
+		
+		}
+
+	}
+	
+	
+	public void addTheLastOfFoundItemsToCart() throws InterruptedException {
+		
+		addToCartButton.click();
+		
+		System.out.println("TEST PASSED: The last found item is added to th cart.");
+		
+		Thread.sleep(5000);
+		
+	}
+	
+	public void clearCart() throws InterruptedException {
+		
+		viewCart.click();
+		Thread.sleep(3000);
+		emptyCartButton.click();
+		Thread.sleep(2000);
+		System.out.println("EmptyCart button is clicked.");
+		EmptyCartConfirmBtn.click();
+		Assert.assertEquals(validateEmptyCart.getText(), "Your cart is empty.");
+		System.out.println("TEST PASSED: The cart is Empty now.");
+	}
+
+}
